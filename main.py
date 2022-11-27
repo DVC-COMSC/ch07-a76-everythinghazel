e4f6447bcfe077da2610747bdab7a535567e4622
Hazel Lee <hyewon1157@gmail.com>
2022-11-27 13:44:34 -0800

    7-6

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
 # ******************************
-# Make your Code
+if len(num1) > len(num2):
+    val = num1
+    num1 = num2
+    num2 = val
+    num3 = num1 + num2
+
+print (num3)
+
+
 # ******************************
 
 # print (num3) 
